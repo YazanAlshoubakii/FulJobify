fd5c87bee06e1fa558e881b54b6da04772af9b10
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-01 00:10:07 +0300

    Setup the basic server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('server working ....');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(5100, () => {
+  console.log('Server Running...');
+});
