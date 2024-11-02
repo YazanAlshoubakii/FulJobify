ab5bf17d6ba01c29f4940b01e911ff9c0b6a9907
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-03 01:35:08 +0300

    Setup morgan and .env packages and setup json middleware

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 
 const app = express();
+import morgan from 'morgan';
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5100, () => {
+app.post('/', (req, res) => {
+  res.json({ message: 'data received', data: { message: req.body } });
+});
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
   console.log('Server Running...');
 });
