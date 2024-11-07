a9c47c53cc47f8a4429752583aa93a6554016019
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-08 01:07:58 +0300

    Add A Edit Route

--- server.js
+++ server.js
@@ -54,6 +54,26 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// Modify a JOb
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    res.status(400).json({ msg: 'please enter company and position!' });
+    return;
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    res.status(404).json({ msg: `no job with this id ${id}` });
+    return;
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+});
+
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log('Server Running...');
