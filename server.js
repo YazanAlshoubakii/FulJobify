a0d63bd86a0f4347a8742219bb30abd47443851c
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-08 01:23:33 +0300

    Add Delete Route

--- server.js
+++ server.js
@@ -74,6 +74,19 @@ app.patch('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ msg: 'job modified', job });
 });
 
+// Delete a JOb
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    res.status(404).json({ msg: `no job with this id ${id}` });
+    return;
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).json({ msg: 'job is Deleted' });
+});
+
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log('Server Running...');
