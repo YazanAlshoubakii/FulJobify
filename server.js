576c14670ec6a5343d2fe436e24d4a43997a5b2d
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-08 00:28:17 +0300

    Add Get All Jobs Route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import { nanoid } from 'nanoid';
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+];
 
 const app = express();
 import morgan from 'morgan';
@@ -18,6 +24,12 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   res.json({ message: 'data received', data: { message: req.body } });
 });
+
+// Get All Jobs
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
+});
+
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log('Server Running...');
