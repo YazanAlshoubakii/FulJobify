82b04ed1f410e9d46cba702f292b948e5d123602
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-08 00:33:30 +0300

    Add Create Job Route

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ app.get('/api/v1/jobs', (req, res) => {
   res.status(200).json({ jobs });
 });
 
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    res.status(400).json({ msg: 'please enter company and position!' });
+    return;
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
+});
+
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log('Server Running...');
