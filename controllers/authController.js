1b22034c13d40876fbedc6657c613d00a27b9bf2
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-18 01:27:32 +0300

    Hashing Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'User Created' });
 };
 
 export const login = async (req, res) => {
