d88612c75673d36260ce4bc94623d3b8c90f9e0f
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-11-22 23:04:18 +0300

    add update user controller and validation middleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,8 +5,9 @@ import {
   getCurrentUser,
   updateUser,
 } from '../controllers/userController.js';
+import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', getApplicationStats);
-router.patch('/update-user', updateUser);
+router.patch('/update-user', validateUpdateUserInput, updateUser);
 
 export default router;
