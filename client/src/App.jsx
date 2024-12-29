1610797f0cae03fb7ce6a15dad4b67e0c355277c
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-12-29 23:55:19 +0300

    add job submission action to AddJob page with success , error notifications and add job functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import {
 
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { action as addJobAction } from './pages/AddJob';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 
 export const checkDefaultTheme = () => {
@@ -38,7 +39,7 @@ const router = createBrowserRouter([
         element: <DashboardLayout />,
         loader: dashboardLoader,
         children: [
-          { index: true, element: <AddJob /> },
+          { index: true, element: <AddJob />, action: addJobAction },
           { path: 'stats', element: <Stats /> },
           { path: 'all-jobs', element: <AllJobs /> },
           { path: 'profile', element: <Profile /> },
